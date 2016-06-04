1c2c9e043b0cb7e2fd35b8f7c6e000037edec23a
Ben <codeandcats@gmail.com>
2016-06-04 12:49:41 +1000

    Made UI.ts a wrapper for the existing legacy UI.js

--- TypeScript/App/UI.ts
+++ TypeScript/App/UI.ts
@@ -1,17 +1,12 @@
-import * as $ from 'jquery';
+// This file is a TypeScript wrapper around the legacy js UI service.
+// By wrapping it with an actual .ts file instead of just a typings file (.d.ts) we
+// are able to import this file as a dependency in other typescript files.
+// Eventually once all the legacy js service consumers are converted over to TypeScript 
+// then one can start converting the services over to be fully implemented in TypeScript too.
+import { getGlobal } from './Interop';
 
-export type MessageType = 'info' | 'success' | 'warning' | 'danger';
+export type MessageType = 'info' | 'success' | 'warning' | 'danger'; 
 
-export function showMessage(message: string, type: MessageType = 'success') {
-	const $message = $(`<div class="alert alert-${type}" role="alert"></div>`);
-	
-	$message.text(message);
-	
-	const $close = $('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
-	
-	$close.click(() => $message.remove());
-	
-	$message.append($close);
-	
-	$('#message-list').append($message);
-}
+var UI = getGlobal('UI');
+
+export const showMessage = <(message: string, type: MessageType) => void>UI.showMessage;
